2f25804ac70b5f1f73d6b60b582476096f8c41bc
Richard Schaubeck <richard.schaubeck@gmail.com>
2019-03-20 17:20:09 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -2,5 +2,13 @@
 import React from 'react';
 
 class Keypad extends React.Component {
-  
+  logMessage = () => {
+    console.log('Entering password...')
+  }
+
+  render() {
+    return(
+      <input type="password" />
+    )
+  }
 }
